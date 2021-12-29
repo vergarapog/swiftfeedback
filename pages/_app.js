6a9285670122866a45087491643c57ec519506a8
vergarapog <201801166@iacademy.edu.ph>
2021-12-29 17:12:57 +0800

    added features until dashboard ep

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "../styles/globals.css"
+import { ChakraProvider } from "@chakra-ui/react"
 
-import { ProvideAuth } from "../lib/auth"
+import { AuthProvider } from "../lib/auth"
+
+import theme from "styles/theme"
+import "styles/fonts.css"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   )
 }
 
