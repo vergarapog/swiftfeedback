fab2be167f8706cb33b65c267c63d348c102937c
vergarapog <201801166@iacademy.edu.ph>
2021-12-29 17:13:13 +0800

    added features until dashboard ep

---
+++ components/EmptyState.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Heading, Box, Text, Button, VStack } from "@chakra-ui/react"
+
+import DashboardShell from "./DashboardShell"
+
+const EmptyState = () => (
+  <DashboardShell>
+    <VStack
+      width="100%"
+      backgroundColor="white"
+      borderRadius={4}
+      p={8}
+      spacing={8}
+    >
+      <VStack spacing={3} justify="center" w="full" textAlign="center">
+        <Heading size="lg">You haven't added any sites</Heading>
+        <Text>Welcome 👋 Let's get started. </Text>
+      </VStack>
+      <Button variant="solid" size="md">
+        Add Your First Site
+      </Button>
+    </VStack>
+  </DashboardShell>
+)
+
+export default EmptyState
